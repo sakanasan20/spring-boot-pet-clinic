b691bcd0d3ec8141ae8fe10ba4f49ec2cdf6b5be
nick.chen <sakanasan20@gmail.com>
2022-06-21 13:39:48 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetClinicApplication.class, args);
+	}
+
+}
