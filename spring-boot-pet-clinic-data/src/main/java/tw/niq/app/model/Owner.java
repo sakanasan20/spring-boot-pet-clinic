81dd10192e5d385f29b9667e8051f46bdf5504af
niq <sakanasan20@gmail.com>
2022-06-22 11:35:21 +0800

    Add ID to model objects (#13)

--- spring-boot-pet-clinic-data/src/main/java/tw/niq/app/model/Owner.java
+++ spring-boot-pet-clinic-data/src/main/java/tw/niq/app/model/Owner.java
@@ -1,5 +1,7 @@
 package tw.niq.app.model;
 
 public class Owner extends Person {
+
+	private static final long serialVersionUID = -7523451833177686859L;
 	
 }
--- spring-boot-pet-clinic-data/src/main/java/tw/niq/app/model/Pet.java
+++ spring-boot-pet-clinic-data/src/main/java/tw/niq/app/model/Pet.java
@@ -3,9 +3,13 @@ package tw.niq.app.model;
 import java.time.LocalDate;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class Pet {
+@EqualsAndHashCode(callSuper = true)
+public class Pet extends BaseEntity {
+
+	private static final long serialVersionUID = -5046320596108888534L;
 
 	private PetType petType;
 	
