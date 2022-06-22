de7e331ce3c928760c20199937979fc16864a829
nick.chen <sakanasan20@gmail.com>
2022-06-22 15:11:24 +0800

    Create owner index page and controller

---
+++ spring-boot-pet-clinic-web/src/main/java/tw/niq/app/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package tw.niq.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+	@RequestMapping({"", "/", "/index", "/index.html"})
+	public String listOwners() {
+		return "owners/index";
+	}
+	
+}
