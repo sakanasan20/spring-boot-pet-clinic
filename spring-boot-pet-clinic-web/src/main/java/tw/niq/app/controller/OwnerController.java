9a60d0c0decb6e8df143d3d37e921df756ea4e81
niq <sakanasan20@gmail.com>
2022-06-23 14:04:03 +0800

    List all owners on owner index page (#32)

--- spring-boot-pet-clinic-web/src/main/java/tw/niq/app/controller/OwnerController.java
+++ spring-boot-pet-clinic-web/src/main/java/tw/niq/app/controller/OwnerController.java
@@ -1,14 +1,26 @@
 package tw.niq.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.niq.app.service.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 	
