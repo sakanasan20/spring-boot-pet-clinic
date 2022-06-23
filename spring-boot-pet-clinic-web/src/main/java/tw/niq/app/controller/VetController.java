cf3bfd0a5a73b4d457452ed21c0a853023efb7d5
niq <sakanasan20@gmail.com>
2022-06-23 14:55:27 +0800

    List all vets on vet index page (#33)

--- spring-boot-pet-clinic-web/src/main/java/tw/niq/app/controller/VetController.java
+++ spring-boot-pet-clinic-web/src/main/java/tw/niq/app/controller/VetController.java
@@ -1,13 +1,25 @@
 package tw.niq.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.niq.app.service.VetService;
 
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 	
